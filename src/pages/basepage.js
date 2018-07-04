9ef6ed49d03002e3a881d2add563ffb9689e1f98
longyinzaitian <1536132397@qq.com>
2018-07-04 15:54:48 +0800

    add main pages ui

---
+++ src/pages/basepage.js
@@ -0,0 +1,39 @@
+
+import wepy from 'wepy'
+import testMixin from '../mixins/test'
+
+export default class BasePage extends wepy.page {
+  config = {
+    
+  }
+  components = {
+    
+  }
+
+  mixins = [testMixin]
+
+  data = {
+    userInfo: {
+      nickName: '加载中...'
+    }
+  }
+
+  computed = {
+  }
+
+  methods = {
+    
+  }
+
+  onLoad() {
+   
+  }
+
+  onShareAppMessage(res) {
+    return {
+      title: 'Mini-Music:超级好听的在线歌曲~',
+      path: '/page/network',
+      imageUrl: '../images/logo.png'
+    }
+  }
+}
