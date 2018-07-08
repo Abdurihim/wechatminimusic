782797257b40e7789db516c60b39fbbd02006df4
longyinzaitian <1536132397@qq.com>
2018-07-08 18:00:38 +0800

    add network music list

--- src/utils/http-request.js
+++ src/utils/http-request.js
@@ -1,27 +1,18 @@
 import wepy from 'wepy'
 export default class HttpRequest {
 	
-	static getMusicList = (type) => {
+	static getMusicList = (type, offset) => {
 		let size = 20;
-		wepy.request({
-			url: "https://tingapi.ting.baidu.com/v1/restserver/ting",
+		return wepy.request({
+			url: 'https:\/\/tingapi.ting.baidu.com/v1/restserver/ting',
 			method: "GET",
 			data: {
-				"type": type,
-				"method": "baidu.ting.billboard.billList",
-				"size": size,
-				"offset": (size - 20)
+				type: type,
+				method: 'baidu.ting.billboard.billList',
+				size: size,
+				offset: offset
 			},
 			dataType: "json",
-			success: function(res) {
-				console.log(res);
-			},
-			fail: function(res) {
-				console.log(res);
-			},
-			complete: function(res) {
-				console.log(res);
-			}
 		});
 	}
 }
